2bdb76a7e64bc692211bf6f810f155ed4d079f24
Ondřej Běhávka <ondrej.behavka@stackit.cloud>
2025-12-02 08:39:49 +0100

    day 2 solution

--- lib/day_02.ex
+++ lib/day_02.ex
@@ -1,52 +1,62 @@
-defmodule Day01 do
+defmodule Day02 do
   def part1(path) do
     File.read!(path)
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn dir ->
-      dir
-      |> String.replace("L", "-")
-      |> String.replace("R", "")
-      |> String.to_integer()
-    end)
-    |> Enum.reduce({50, 0}, fn dir, {old, count} ->
-      curr = Integer.mod(old + dir, 100)
+    |> String.split(",", trim: true)
+    |> Enum.flat_map(fn range_str ->
+      [lower, higher] =
+        range_str
+        |> String.split("-", trim: true)
+        |> Enum.map(&String.to_integer/1)
 
-      if curr == 0 do
-        {curr, count + 1}
-      else
-        {curr, count}
-      end
+      lower..higher
     end)
-    |> elem(1)
+    |> Enum.filter(&is_mirrored?/1)
+    |> Enum.sum()
   end
 
   def part2(path) do
     File.read!(path)
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn dir ->
-      dir
-      |> String.replace("L", "-")
-      |> String.replace("R", "")
-      |> String.to_integer()
-    end)
-    |> Enum.reduce({50, 0}, fn dir, {current_pos, total_zeros} ->
-      target = current_pos + dir
-      path_range =
-        if dir > 0 do
-          Range.new(current_pos + 1, target, 1)
-        else
-          Range.new(current_pos - 1, target, -1)
-        end
-      new_zeros =
-        path_range
-        |> Enum.count(fn step -> Integer.mod(step, 100) == 0 end)
+    |> String.split(",", trim: true)
+    |> Enum.flat_map(fn range_str ->
+      [lower, higher] =
+        range_str
+        |> String.split("-", trim: true)
+        |> Enum.map(&String.to_integer/1)
 
-      new_pos = Integer.mod(target, 100)
-
-      {new_pos, total_zeros + new_zeros}
+      lower..higher
     end)
-    |> elem(1)
+    |> Enum.filter(&is_periodic?/1)
+    |> Enum.sum()
   end
 
+  defp is_periodic?(number) do
+    str = Integer.to_string(number)
+    len = String.length(str)
+    if len < 2 do
+      false
+    else
+      1..div(len, 2)
+      |> Enum.any?(fn chunk_size ->
+        if rem(len, chunk_size) == 0 do
+          chunk = String.slice(str, 0, chunk_size)
+          count = div(len, chunk_size)
+          new = String.duplicate(chunk, count)
+          new == str
+        else
+          false
+        end
+      end)
+    end
+  end
 
+  defp is_mirrored?(number) do
+     s = Integer.to_string(number)
+      len = String.length(s)
+
+      rem(len, 2) == 0 &&
+        (
+          {head, tail} = String.split_at(s, div(len, 2))
+          head == tail
+        )
+  end
 end
--- test/day_02_test.exs
+++ test/day_02_test.exs
@@ -4,20 +4,20 @@ defmodule Day02Test do
   import AocHelper
 
   test "part 1 example" do
-    assert Day02.part1("input/02_ex.txt") == 3
+    assert Day02.part1("input/02_ex.txt") == 1227775554
   end
 
   test "part 1 answer" do
     answer = solve_and_print "Day 02: Part 1", Day02.part1("input/02.txt")
-    assert answer == 1180
+    assert answer == 19574776074
   end
 
-  # test "part 2 example" do
-  #   assert Day02.part2("input/02_ex.txt") == 6
-  # end
+  test "part 2 example" do
+    assert Day02.part2("input/02_ex.txt") == 4174379265
+  end
 
-  # test "part 2 answer" do
-  #   answer = solve_and_print "Day 02: Part 2", Day02.part2("input/02.txt")
-  #   assert answer == 6892
-  # end
+  test "part 2 answer" do
+    answer = solve_and_print "Day 02: Part 2", Day02.part2("input/02.txt")
+    assert answer == 25912654282
+  end
 end
